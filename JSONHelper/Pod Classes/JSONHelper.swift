9d0d48738f22382a992233da796259a1017ae512
Baris Sencan <run32a50mch@gmail.com>
2014-08-29 15:54:45 -0700

    Added license into source file.

--- JSONHelper/Pod Classes/JSONHelper.swift
+++ JSONHelper/Pod Classes/JSONHelper.swift
@@ -1,9 +1,33 @@
 //
 //  JSONHelper.swift
-//  JSONHelper
 //
-//  Created by Baris Sencan on 28/08/14.
-//  Copyright (c) 2014 Baris Sencan. All rights reserved.
+//  Version 1.0.1
+//
+//  Created by Baris Sencan on 28/08/2014.
+//  Copyright 2014 Baris Sencan
+//
+//  Distributed under the permissive zlib license
+//  Get the latest version from here:
+//
+//  https://github.com/isair/JSONHelper
+//
+//  This software is provided 'as-is', without any express or implied
+//  warranty.  In no event will the authors be held liable for any damages
+//  arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//  claim that you wrote the original software. If you use this software
+//  in a product, an acknowledgment in the product documentation would be
+//  appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be
+//  misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
 //
 
 import Foundation
