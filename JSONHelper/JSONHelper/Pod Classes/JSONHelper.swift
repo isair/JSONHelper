8b79817cacc02fb0cba956f09526461bf37cb6c3
Baris Sencan <run32a50mch@gmail.com>
2014-08-28 18:18:27 -0700

    Initial commit.
    This project does nothing for now.

---
+++ JSONHelper/JSONHelper/Pod Classes/JSONHelper.swift
@@ -0,0 +1,9 @@
+//
+//  JSONHelper.swift
+//  JSONHelper
+//
+//  Created by Baris Sencan on 28/08/14.
+//  Copyright (c) 2014 Baris Sencan. All rights reserved.
+//
+
+import Foundation
---
+++ JSONHelper/JSONHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONHelper
+//
+//  Created by Baris Sencan on 28/08/14.
+//  Copyright (c) 2014 Baris Sencan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
