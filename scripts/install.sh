53fac562701a516b5a38e9187f7469f027241707
Baris Sencan <baris.sncn@gmail.com>
2017-05-09 09:46:33 -0700

    Add various scripts to make life easier
    Additional changes:
    - [Xcode] Clear development team, set code signing to automatic
    - Remove changelog
    - Strict version control of environment and dependencies

---
+++ scripts/install.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+# Check project requirements
+
+check_required_bin() {
+  if ! hash $1 2>/dev/null; then
+    echo "$1 not found."
+    exit 1
+  fi
+}
+
+check_requirements() {
+  check_required_bin rbenv
+  check_required_bin ruby-build
+  check_required_bin xcodebuild
+}
+
+printf "Checking project requirements... "
+check_requirements
+echo "done."
+
+# Initialize project
+
+cd "$(dirname "$0")/.."
+
+RUBY_VERSION=`cat .ruby-version`
+rbenv install -s $RUBY_VERSION
+
+gem install bundler
+rbenv rehash
+
+bundle install --path vendor/bundle
